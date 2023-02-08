fb0d3c20fba18481d09aa088fc51ec2f291b7f29
Chadaporn Thaikana <thaikana2544@gmail.com>
2023-02-08 23:30:12 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a1ef0e679a93b071a969b977b32d0747'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '138e3029-1fa3-4174-99fa-579a6d98292f'
+};
