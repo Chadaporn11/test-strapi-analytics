c64c89339502beea132620fd9368949bf6675816
Chadaporn Thaikana <thaikana2544@gmail.com>
2023-02-13 14:53:05 +0700

    getAnalytics Total User

--- plugins/dashboard/admin/src/containers/HomePage/index.js
+++ plugins/dashboard/admin/src/containers/HomePage/index.js
@@ -19,7 +19,7 @@ import './index.css';
 // import Chart from "chart.js/auto";
 // import { Pie } from "react-chartjs-2";
 import ApexCharts from 'apexcharts'
-import { Card, DatePicker, TimePicker, Space, Select } from 'antd';
+import { Card, DatePicker, TimePicker, Space, Select, Form, Button } from 'antd';
 import { TeamOutlined } from '@ant-design/icons';
 const { Option } = Select;
 const PickerWithType = ({ type, onChange }) => {
@@ -27,79 +27,82 @@ const PickerWithType = ({ type, onChange }) => {
   if (type === 'date') return <DatePicker onChange={onChange} />;
   return <DatePicker picker={type} onChange={onChange} />;
 };
+import { getAnalyticsRealTime, getAnalyticsTotal } from './function/analyticsapi'
 
 
 
 const HomePage = () => {
 
+  const [form] = Form.useForm()
   const [statusSelect, setStatusSelect] = useState(false)
   const [dataSelect, setDataSelect] = useState('')
   const [type, setType] = useState('date');
-  const [token, setToken] = useState('')
+  // const [token, setToken] = useState('')
   const [countUser, setCountUser] = useState(0)
+  const [totalUser, setTotalUser] = useState(0)
 
 
   //API//
 
 
-  const AuthGoogle = async () => {
-    //Get Data
+  // const AuthGoogle = async () => {
+  //   //Get Data
+  //   // setToken(JSON.parse(sessionStorage.getItem("jwtToken")))
+  //   // const token = JSON.parse(sessionStorage.getItem("jwtToken"))
+  //   const requestOptions = {
+  //     method: "GET",
+  //     headers: { Authorization: `Bearer ${JSON.parse(sessionStorage.getItem("jwtToken"))}`, "Content-Type": "application/json" },
+  //   };
+  //   const analyticstoken = await fetch(`http://localhost:1337/analyticsapis/token`, requestOptions)
+  //     .then((response) => response.json())
+  //     .then((res) => {
+  //       return res
+  //     });
+  //   console.log('Token::', analyticstoken)
+  //   const CLIENT_EMAILV4 = "test-getanalytics4@test-getanalysicv4.iam.gserviceaccount.com"
+  //   const PRIVATE_KEYV4 = "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDEHaCRAjOIrueA\nIky0as7aTqLkilsdU9fcOzPQeWG5jUzSUoyjVIKCdyib97wJnFmx1q+vc9LRG26N\nIcOEfp/UqVQgL9QYqOy4HkiQA/ru0fWg01MWrbJA28OumI8WojG/0FIzN/7LKJ5p\nFFvYnS4sXnmrnW57PcyaFsLaGu8TdavssNtTll3KwcBbYnXAUEFvqoI9Oi/aQ+JF\nIEjtmcAtcJDwefzZBxe5uzU4RY8glH99eoQvi6JbTaW48WUuOp0bf+tEXx997Erw\nJr6zPMYi/dLOeL6DUEx7p6sGGs4TKh9bVzVm0oQ0324riC3KZpHY9MVO9MQsXr+s\nvqVY7zChAgMBAAECggEAPjBe8FzQVrGP+5Pk2vlbquJwySQOjJg/xTiIz70N/jcO\ne0tuCYUM9a4vlHXUjP+leb1obKy/10tpnRtt0qAMsvUXFjXUfhVjK2d+/xwkAINX\n7q3UlUZzVhnaTY1XIohAWmEHD/LbuzSgNxYboe8F60/yKTd9B1ure9ln5J3R3ktO\nGSXvmW6PagbpyvVkTlyH1TDg9Jz24pRtJ2O8t0M63i5IErwbOPGL5ukmkypPsNL7\nkoZjFUA0RN9682uyfvaiTisHcsG0ug9pV+HIkhvVGmnZTvAIQL64Mr6ghXbsUGzf\n6yXII5L+Pd9/TskOfUAc8iN0nM624Dtz3+3r8qDeOQKBgQDlqybkzMs9WNVvmysZ\no7FqIYqAE0SplWBTzfazKFMKCjzNyZuahb2qysfvAXLHWJfGASO9oySlY52c5hha\nSlSpAfCdQRx+7bMbElpm2nDstrNdJKCiFb3JnwjKic3pxcTlVdTfoOQyAXnYFhK8\nORsGp4z+UQQW4I09/BW0DTpwfwKBgQDambBJ2wyNKNi7QcQeTST8Sc4KUy1hz/i5\nA+zz63a6D99zzyKaiIq6HVQHaUsFiAvx9YcE4j7PxUJnPrlWDHYDaftwoUQP/4F0\nKwU5RidVyTvBQ4k7+mI8ezi3r6AeCL4vgFn22xrEmltY9OjAmSviFWN05jLa4xVt\nvfEOQv/O3wKBgQCEdMowqOAKWIJ4JJCd7+dxYzjCltpBx9HPY3kFaJtDrhXVRZIF\nc16o8tyOPlKZH8IgwyV+yGlpLWOISrf+0uGyu1ivCQ9LMQHb+iDDMvZvvwsBDA/M\niydy8dKbJRDp41KkRXVJKDyTjoBcHJbfkTvCAb3yKn1mSEmNVyaZOgvbNwKBgQDN\nob8caosvClp+JAppeqYtEPxZ6A6LsUhGOnQeq8PemOnZFeN2PLVLCCWwZxLkXCro\na8+b/3uYlPW3C2DqmgQ1h++37muJJQ/QiFt8mgqMfyTP00j3+7uHK16aJAJZ3l2R\nb4qxVUCj6pO9ZXzBGBUYsogBQj5aez0AI6nrgYkDPwKBgQCunPitZtSQHJHaEXMk\nlSMYY4G1SvoSFw/2TJbtUUMtX+YIMGEphbkgFJM/Btk5EvTWSdmmycfJW7F7XJsh\nVapjVOIRywIa+Wf5Z+617fqYKqi/4mJOs99c1FFcDUEt06pbb2XBB5k28LYnlth7\nHwnoQT24B7BGle0Yt1G3oxGzxQ==\n-----END PRIVATE KEY-----\n"
+  //   const VIEW_IDV4 = "352577750"
+  //   const API_KEYV4 = "AIzaSyARCgWSFd6X4AgjlfNwgPUmGdBeanVGIK4"
+  //   let data = {
+  //     "metrics": [
+  //       {
+  //         "name": "activeUsers"
+  //       }
+  //     ]
+  //   }
+  //   let x = 0;
+  //   const requestOptionsPost = {
+  //     method: "POST",
+  //     headers: { Authorization: `Bearer ${analyticstoken.access_token}`, "Content-Type": "application/json" },
+  //     body: JSON.stringify(data),
 
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjc2MDA2OTYzLCJleHAiOjE2Nzg1OTg5NjN9.3OB2av8hSUsZZW8Q-ddJF7-kkKfeXKRT9jN63_Jk-H8'
-    const requestOptions = {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}`, "Content-Type": "application/json" },
-    };
-    const analyticstoken = await fetch(`http://localhost:1337/analyticsapis/token`, requestOptions)
-      .then((response) => response.json())
-      .then((res) => {
-        return res
-      });
-    console.log('Token::', analyticstoken)
-    const CLIENT_EMAILV4 = "test-getanalytics4@test-getanalysicv4.iam.gserviceaccount.com"
-    const PRIVATE_KEYV4 = "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDEHaCRAjOIrueA\nIky0as7aTqLkilsdU9fcOzPQeWG5jUzSUoyjVIKCdyib97wJnFmx1q+vc9LRG26N\nIcOEfp/UqVQgL9QYqOy4HkiQA/ru0fWg01MWrbJA28OumI8WojG/0FIzN/7LKJ5p\nFFvYnS4sXnmrnW57PcyaFsLaGu8TdavssNtTll3KwcBbYnXAUEFvqoI9Oi/aQ+JF\nIEjtmcAtcJDwefzZBxe5uzU4RY8glH99eoQvi6JbTaW48WUuOp0bf+tEXx997Erw\nJr6zPMYi/dLOeL6DUEx7p6sGGs4TKh9bVzVm0oQ0324riC3KZpHY9MVO9MQsXr+s\nvqVY7zChAgMBAAECggEAPjBe8FzQVrGP+5Pk2vlbquJwySQOjJg/xTiIz70N/jcO\ne0tuCYUM9a4vlHXUjP+leb1obKy/10tpnRtt0qAMsvUXFjXUfhVjK2d+/xwkAINX\n7q3UlUZzVhnaTY1XIohAWmEHD/LbuzSgNxYboe8F60/yKTd9B1ure9ln5J3R3ktO\nGSXvmW6PagbpyvVkTlyH1TDg9Jz24pRtJ2O8t0M63i5IErwbOPGL5ukmkypPsNL7\nkoZjFUA0RN9682uyfvaiTisHcsG0ug9pV+HIkhvVGmnZTvAIQL64Mr6ghXbsUGzf\n6yXII5L+Pd9/TskOfUAc8iN0nM624Dtz3+3r8qDeOQKBgQDlqybkzMs9WNVvmysZ\no7FqIYqAE0SplWBTzfazKFMKCjzNyZuahb2qysfvAXLHWJfGASO9oySlY52c5hha\nSlSpAfCdQRx+7bMbElpm2nDstrNdJKCiFb3JnwjKic3pxcTlVdTfoOQyAXnYFhK8\nORsGp4z+UQQW4I09/BW0DTpwfwKBgQDambBJ2wyNKNi7QcQeTST8Sc4KUy1hz/i5\nA+zz63a6D99zzyKaiIq6HVQHaUsFiAvx9YcE4j7PxUJnPrlWDHYDaftwoUQP/4F0\nKwU5RidVyTvBQ4k7+mI8ezi3r6AeCL4vgFn22xrEmltY9OjAmSviFWN05jLa4xVt\nvfEOQv/O3wKBgQCEdMowqOAKWIJ4JJCd7+dxYzjCltpBx9HPY3kFaJtDrhXVRZIF\nc16o8tyOPlKZH8IgwyV+yGlpLWOISrf+0uGyu1ivCQ9LMQHb+iDDMvZvvwsBDA/M\niydy8dKbJRDp41KkRXVJKDyTjoBcHJbfkTvCAb3yKn1mSEmNVyaZOgvbNwKBgQDN\nob8caosvClp+JAppeqYtEPxZ6A6LsUhGOnQeq8PemOnZFeN2PLVLCCWwZxLkXCro\na8+b/3uYlPW3C2DqmgQ1h++37muJJQ/QiFt8mgqMfyTP00j3+7uHK16aJAJZ3l2R\nb4qxVUCj6pO9ZXzBGBUYsogBQj5aez0AI6nrgYkDPwKBgQCunPitZtSQHJHaEXMk\nlSMYY4G1SvoSFw/2TJbtUUMtX+YIMGEphbkgFJM/Btk5EvTWSdmmycfJW7F7XJsh\nVapjVOIRywIa+Wf5Z+617fqYKqi/4mJOs99c1FFcDUEt06pbb2XBB5k28LYnlth7\nHwnoQT24B7BGle0Yt1G3oxGzxQ==\n-----END PRIVATE KEY-----\n"
-    const VIEW_IDV4 = "352577750"
-    const API_KEYV4 = "AIzaSyARCgWSFd6X4AgjlfNwgPUmGdBeanVGIK4"
-    let data = {
-      "metrics": [
-        {
-          "name": "activeUsers"
-        }
-      ]
-    }
-    let x = 0;
-    const requestOptionsPost = {
-      method: "POST",
-      headers: { Authorization: `Bearer ${analyticstoken.access_token}`, "Content-Type": "application/json" },
-      body: JSON.stringify(data),
+  //   };
+  //   const result = await fetch(`https://analyticsdata.googleapis.com/v1beta/properties/${VIEW_IDV4}:runRealtimeReport?key=${API_KEYV4}`, requestOptionsPost)
+  //     .then((response) => response.json())
+  //     .then((res) => {
+  //       return res
+  //     });
+  //   console.log("Result:: ", result)
+  //   let ans = Object.keys(result).find(key => key === 'rows') ? true : false;
+  //   // let test = result.data.rows !== null || result.data.rows !== undefined ? true : false;
+  //   console.log("Ans:: ", ans)
+  //   if (ans) {
+  //     x = result.rows[0].metricValues[0].value
+  //     console.log('Have::', x)
+  //     setCountUser(x)
 
-    };
-    const result = await fetch(`https://analyticsdata.googleapis.com/v1beta/properties/${VIEW_IDV4}:runRealtimeReport?key=${API_KEYV4}`, requestOptionsPost)
-      .then((response) => response.json())
-      .then((res) => {
-        return res
-      });
-    console.log("Result:: ", result)
-    let ans = Object.keys(result).find(key => key === 'rows') ? true : false;
-    // let test = result.data.rows !== null || result.data.rows !== undefined ? true : false;
-    console.log("Ans:: ", ans)
-    if (ans) {
-      x = result.rows[0].metricValues[0].value
-      console.log('Have::', x)
-      setCountUser(x)
-
-    } else {
-      x = 0;
-      console.log('DontHave::', x)
-      setCountUser(x)
+  //   } else {
+  //     x = 0;
+  //     console.log('DontHave::', x)
+  //     setCountUser(x)
 
 
-    }
+  //   }
 
-  }
+  // }
 
   setInterval(() => {
-    AuthGoogle()
+    loadData()
 
   }, 20000)
   // console.log('CountUser::', countUser)
@@ -415,14 +418,44 @@ const HomePage = () => {
 
 
   }
+  const loadData = () => {
+    getAnalyticsRealTime().then((res) => {
+      const ans = Object.keys(res).find(key => key === 'rows') ? true : false;
+      if (ans) {
+        setCountUser(res.rows[0].metricValues[0].value)
+      } else {
+        setCountUser(0)
+      }
+    })
+    getAnalyticsTotal().then((res) => {
+      const ans = Object.keys(res).find(key => key === 'rows') ? true : false;
+      if (ans) {
+        setTotalUser(res.rows[0].metricValues[0].value)
+      } else {
+        setTotalUser(0)
+      }
+    })
+  }
 
 
   useEffect(() => {
     loadDataChart()
-    AuthGoogle()
+    loadData()
+
 
   }, [])
+  const onFinish = (values) => {
+    console.log('onFinish', values)
+    // let startDate = values.date?.[0]
+    //   ? dayjs(values.date[0]).format("YYYY-MM-DD")
+    //   : undefined
 
+    // let endDate = values.date?.[1]
+    //   ? dayjs(values.date[1]).format("YYYY-MM-DD")
+    //   : undefined
+
+    // console.log('Test:;', startDate, endDate)
+  };
 
   return (
     <div className='Dashboard-Main'>
@@ -431,7 +464,56 @@ const HomePage = () => {
       </div>
       <div className='Dashboard-container'>
         <div className='col-select'>
-          <DatePicker
+          <Form
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            style={{
+              maxWidth: 600,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            form={form}
+            onFinish={onFinish}>
+            <Form.Item
+              className="font-bold"
+              label="วันที่เริ่ม-สิ้นสุด"
+              name="date"
+            >
+              <DatePicker.RangePicker
+                className="w-full rounded"
+                placeholder={["วันที่เริ่ม", "สิ้นสุด"]}
+              />
+            </Form.Item>
+            <Form.Item className="">
+              <Button
+                type="text"
+                className=" w-full rounded bg-gray text-white hover:bg-red-300"
+                onClick={() => {
+                  form.resetFields();
+                }}
+              >
+                ยกเลิก
+              </Button>
+            </Form.Item>
+            <Form.Item className="">
+              <Button
+                type="text"
+                className=" w-full rounded bg-danger text-white hover:bg-red-300"
+                onClick={() => {
+                  form.submit();
+                }}
+              >
+                ค้นหา
+              </Button>
+            </Form.Item>
+          </Form>
+          {/* <DatePicker
             style={{
               marginRight: '30px',
             }}
@@ -441,7 +523,7 @@ const HomePage = () => {
               marginRight: '30px',
             }}
             picker="month"
-            onChange={onChangeMonth} />
+            onChange={onChangeMonth} /> */}
         </div>
         <div className='col-graph'>
           <Card
@@ -496,7 +578,7 @@ const HomePage = () => {
                 <TeamOutlined style={{ fontSize: '60px', color: 'white' }} />
                 <h1 style={{
                   color: 'white',
-                }}>150</h1>
+                }}>{totalUser}</h1>
                 <p style={{
                   color: 'white',
                 }}>คน</p>
